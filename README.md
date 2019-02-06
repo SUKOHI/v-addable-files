e242365f564bde0e6497a3c56c2097b3c0e0048e
SUKOHI <capilano.sukohi@gmail.com>
2019-02-06 20:11:43 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # v-addable-files
 A Vue directive that allows you to input multiple files.
+
+# Installation
+
+    npm i v-addable-files --save
+    
+# Usage
+
+## Basic way
+
+    <input type="file" v-addable-files="files" multiple>
+
+    new Vue({
+        el: '#app',
+        data: {
+            files: []
+        }
+    })
+
+## with Maximum
+
+    <input type="file" v-addable-files="files" max="5" @too-many="onError" multiple>
+
+    new Vue({
+        el: '#app',
+        data: {
+            files: []
+        },
+        methods: {
+            onError: function(e) {
+
+                console.log('Too many!', e.detail)
+
+            }
+        }
+    })
+
+# License
+
+This package is licensed under the MIT License.
+
+Copyright 2018 Sukohi Kuhoh
---
+++ src/v-addable-files.js
@@ -0,0 +1,39 @@
+Vue.directive('addable-files', {
+    bind: (el, binding, vnode) => {
+
+        var max = (el.hasAttribute('max')) ? parseInt(el.max) : -1;
+
+        el.addEventListener('change', function(){
+
+            var key = binding.expression;
+            var currentFiles = vnode.context[key];
+
+            for(var i = 0 ; i < this.files.length ; i++) {
+
+                if(max > 0 && currentFiles.length === max) {
+
+                    var event  = document.createEvent('CustomEvent');
+                    event.initCustomEvent('too-many', false, false, {
+                        el: el,
+                        files: {
+                            current: currentFiles,
+                            input: this.files
+                        }
+                    });
+                    el.dispatchEvent(event);
+                    this.value = '';
+                    return;
+
+                }
+
+                var file = this.files[i];
+                currentFiles.push(file);
+
+            }
+
+            this.value = '';
+
+        });
+
+    }
+});
